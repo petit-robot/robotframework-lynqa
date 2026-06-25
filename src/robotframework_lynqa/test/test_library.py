89634e9713179851fb195a9be149cd4335fb82ed
apallier <alexis.pallier@protonmail.com>
2026-06-25 18:25:55 +0200

    test: fix tz issue on CI

--- src/robotframework_lynqa/test/test_library.py
+++ src/robotframework_lynqa/test/test_library.py
@@ -50,7 +50,8 @@ def listener_probe(monkeypatch, mocker, client):
     """Initialize the listener probe used to capture the library instance."""
     monkeypatch.setenv("API_KEY", API_KEY)
     mock_datetime = mocker.patch("robotframework_lynqa.library.datetime")
-    mock_datetime.now.return_value = datetime(2026, 6, 24, 9, 52, 0, tzinfo=timezone(timedelta(hours=2)))
+    fixed_now = datetime(2026, 6, 24, 9, 52, 0, tzinfo=timezone(timedelta(hours=2)))
+    mock_datetime.now.return_value.astimezone.return_value = fixed_now
     return _CaptureLibInstance()
 
 
